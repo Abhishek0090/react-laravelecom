28306ce668e267730e3c82ac7cbb8fadc20072e2
Abhishek0090 <palabhishek411@gmail.com>
2022-12-05 18:39:21 +0530

    fixed logout issue and admin authentication

--- src/components/frontend/auth/Login.js
+++ src/components/frontend/auth/Login.js
@@ -29,14 +29,14 @@ const Login = () => {
     axios.get('/sanctum/csrf-cookie').then(response => {
         axios.post(`/api/login`,data).then(res=>{
             if(res.data.status===200){
-                localStorage.setItem('auth-token',res.data.token);         
-                localStorage.setItem('auth-name',res.data.username);         
+                localStorage.setItem('auth_token',res.data.token);         
+                localStorage.setItem('auth_name',res.data.username);         
                 swal("Success",res.data.message,"success");
                 Navigate('/');
 
             }else if(res.data.status===401){
                 swal("warning",res.data.message,"warning");     
-                
+
             }else{
                 setLogin({...loginInput,error_list:res.data.validation_errors})
             }
